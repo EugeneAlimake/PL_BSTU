d6e84ca598398b61896acb515e94aecdfcb90d2b
Eugene <90150437+EugeneAlimake@users.noreply.github.com>
2021-10-07 14:21:30 +0300

    Add files via upload

---
+++ 3 лаба/LP_Lab03/LP_Lab03/LP_Lab03.cpp
@@ -0,0 +1,55 @@
+﻿#include <cstdlib>
+#include <iostream>
+using namespace std;
+//EvgeniaNikolaeva2002
+//ЕвгенияНиколаева2002
+//Евгения2002Nikolaeva
+//Windows-1251:
+//45 76 67 65 6E 69 61(Evgenia) 4E 69 6B 6F 6C 61 65 76 61(Nikolaeva) 32 30 30 32(2002)
+//0415 0432 0433 0435 043D 0438 044F(Евгения) 041D 0438 043A 043E 043B 0430 435 0432 0430(Николаева) 32 30 30 32(2002)
+//0415 0432 0433 0435 043D 0438 044F(Евгения)32 30 30 32(2002)4E 69 6B 6F 6C 61 65 76 61(Nikolaeva)
+//UTF-8:
+//d095 dob2 d0b3 d0b5 d0bd d0b8 d18f(Евгения) d09d d0b8 d0ba d0be d0bb d0b0 d0b5 d0b2 d0b0(Николаева) 32 30 30 32(2002)
+//45 76 67 65 6е 69 61(Evgenia) 4е 69 6b 6f 6c 61 65 76 61(Nikolaeva) 32 30 30 32(2002)
+//d095 dob2 d0b3 d0b5 d0bd d0b8 d18f(Евгения)32 30 30 32(2002)4е 69 6b 6f 6c 61 65 76 61(Nikolaeva)
+//UTF-16
+//0415 0432 0433 0435 043D 0438 044F(Евгения) 041D 0438 043A 043E 043B 0430 0435 0432 0430(Николаева) 32 30 30 32(2002)
+//0045 0076 0067 0065 006E 0069 0061(Evgenia) 004E 0069 006B 006F 006C 0061 0065 0076 0061(Nikolaeva) 0032 0030 0030 0032(2002)
+//char rf[] = "EvgeniaNikolaeva2002";
+//char lf[] = "ЕвгенияНиколаева2002";
+//char kf[] = "Евгения2002Nikolaeva";
+//
+//wchar_t Lrf[] = L"EvgeniaNikolaeva2002";
+//wchar_t Llf[] = L"ЕвгенияНиколаева2002";
+//wchar_t Lkf[] = L"Евгения2002Nikolaeva";
+
+char* UpperW1251(char* destination, char* source) {
+	for (int i = 0; i <= strlen(source); i++)
+	{
+		if (source[i] >= 'a' && source[i] <= 'z')
+		{
+			destination[i] = source[i] - 32;
+		}
+		else if (source[i] >= 'а' && source[i] <= 'я')
+		{
+			destination[i] = source[i] - 32;
+		}
+		else if (source[i] == 'ё')
+		{
+			destination[i] = 'Ё';
+		}
+		else
+			destination[i] = source[i];
+	}
+	return destination;
+}
+int main()
+{	setlocale(LC_ALL, "Russian");
+	char source[] = "12345sdfghjlйцгшщё";
+	char destination[] = "1234567890123456789";
+	UpperW1251(destination, source);
+	cout << destination << endl;
+	system("pause");
+	return 0;
+}
+
---
+++ LP_Lab04/LP_Lab04/LP_Lab04.cpp
@@ -0,0 +1,87 @@
+﻿// LP_Lab04.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{//: X= 9 + 13, Y= 10 + 13, Z= 11 + 13, S= 1.0 + 13.
+//4 задание:
+	int lb = sizeof(bool);//1байт-логический
+	int lc = sizeof(char);//1-символьный
+	//5 задание:
+	int ld = sizeof(float);//4-вещественный
+	//6 задание:
+	int lwc = sizeof(wchar_t);//2-расширенный символьный
+	//7 задание:
+	int lshort = sizeof(short);//2-целый
+	int lushort = sizeof(unsigned short);//2-целый(беззнаковый)
+	int lint = sizeof(int);//4-целый
+	int llong = sizeof(long);//8
+	//9 задание:0
+	unsigned short iushort = USHRT_MAX;//мин=0 //ffff
+	unsigned short iuint = UINT_MAX;//мин=0//ffffffff
+	int lilong = sizeof(unsigned long);
+	unsigned long iulong = ULONG_MAX;//мин=0//ffffffff
+	//4 задание
+	bool b1 = false;//00
+	bool b2 = true;//01
+	//5 задание:
+	char P[] = "IJFD123";//f6h
+	int k1 = sizeof(char);
+	//6 задание:
+	wchar_t wc[] = { L'J',L'L',L'V',L'W',L's',L'w',L'q' };//4b
+	int h = sizeof(wc);
+	//7 задание:
+	short ishort1 = 0x1234;//16
+	short ishort2 = -22;//(-16)ffea
+	int a = sizeof(ishort1);
+	//8 задание:
+	short ishortMAX = SHRT_MAX;//32767-7fff
+	short ishortMIN = SHRT_MIN;//-32767
+	//10 задание:
+	int iint1[] = { 1,2,3,4 };//17 
+	int po = sizeof(iint1);
+	int iint2 = -23;//-17
+	int iintMAX = INT_MAX;//7fffffff 
+	int iintMIN = INT_MIN;
+	//13 задание:
+	long ilong1 = 24;//18
+	long ilong2 = -24;//-18
+	long ilongMAX = LONG_MAX;//7fffffff
+	long ilongMIN = LONG_MIN;
+	//17 Задание:
+	float f1[] = { 1,2,234 };//E
+	int i1 = 25;
+	double d1 = 123.45;
+	char c1 = 'A';
+	wchar_t w1 = 'J';
+	int* pi1 = &i1;
+	double* pd1 = &d1;
+	char* pc1 = &c1;
+	wchar_t* pw1 = &w1;
+	cout << pi1 << endl;
+	cout << pd1 << endl;
+	cout << pc1 << endl;
+	cout << pw1 << endl;
+	pi1 = pi1 + 3;
+	pd1 = pd1 + 4;
+	pc1 = pc1 + 5;
+	pw1 = pw1 + 6;
+	cout << pi1 << endl;
+	cout << pd1<<endl;
+	cout << pc1 << endl;
+	cout << pw1 << endl;
+ double dob = 9;
+ double* ukazzatel5 = &dob;//2240
+ ukazzatel5 += 3;
+ double& sialka5 = dob;
+
+ void* linker;
+ linker = &b1;
+ linker = &dob;
+ system("pause");
+ return 0;
+
+}
+
+
